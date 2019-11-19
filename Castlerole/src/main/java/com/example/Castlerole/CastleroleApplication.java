5753b086ec0cc8269fff360b05664a9ca51587bb
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2019-11-19 16:37:23 +0100

    login met database
    https://www.javainuse.com/spring/ang7-jwt
    
    tutorial

---
+++ Castlerole/src/main/java/com/example/Castlerole/CastleroleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Castlerole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleroleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleroleApplication.class, args);
+	}
+
+}
