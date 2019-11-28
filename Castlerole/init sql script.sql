5165064914bdb2ac522a979e3b324e1b66d440c4
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2019-11-28 19:32:27 +0100

    updated database script

--- Castlerole/init sql script.sql
+++ Castlerole/init sql script.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS `castleroledb`;
+nodesDROP DATABASE IF EXISTS `castleroledb`;
 CREATE DATABASE `castleroledb`;
 USE `castleroledb`;
 
@@ -6,5 +6,21 @@ USE `castleroledb`;
 CREATE TABLE `user` (
 		`id` int not null AUTO_INCREMENT PRIMARY KEY,
         `username` varchar(50) not null,
-        `password` varchar(128) not null
+        `password` varchar(128) not null,
+        `join_date` date,
+        `wood` int,
+        `iron` int,
+        `stone` int,
+        `food` int,
+        `troops` int
+        );
+
+CREATE TABLE `Nodes` (
+		`id` int not null AUTO_INCREMENT PRIMARY KEY,
+        `type` varchar(50) not null,
+        `ownerId` int,
+        `troops` date,
+        `yield_min` int,
+        `yield_max` int,
+        FOREIGN KEY (`ownerId`) REFERENCES user(`id`)
         );
