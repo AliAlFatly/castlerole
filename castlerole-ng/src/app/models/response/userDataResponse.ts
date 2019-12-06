21475b05aaefb0e01f870248d21ae39d7edb6d3a
Rick Bunte <rickbunte2000@gmail.com>
2019-12-06 13:26:58 +0100

    Added user information to the game page

---
+++ castlerole-ng/src/app/models/response/userDataResponse.ts
@@ -0,0 +1,10 @@
+export class userDataResponse{
+    username: string;
+    coordinateX: number;
+    coordinateY: number;
+    wood: number;
+    iron: number;
+    stone: number;
+    food: number;
+    troops: number;
+}
