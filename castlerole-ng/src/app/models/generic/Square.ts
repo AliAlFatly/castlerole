37bafb9f1fc5a9f2d1eeb0e3c35eb6de35ef8ca7
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2019-12-06 16:04:40 +0100

    set apis to get instead of post

--- castlerole-ng/src/app/models/generic/Square.ts
+++ castlerole-ng/src/app/models/generic/Square.ts
@@ -1,7 +1,7 @@
-export class square{
+export class Square{
   constructor(private ctx: any) {}
 
   draw(x: number, y: number, z: number) {
-      this.ctx.fillRect(z * x, z * y, z, z);
-    }
+    this.ctx.fillRect(z * x, z * y, z, z);
+  }
 }
