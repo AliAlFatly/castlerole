df5d21b975fcd13821648f0aa25b744979200c28
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2019-12-07 12:47:11 +0100

    added grid drawing functionality in angular

--- Castlerole/src/main/java/com/example/Castlerole/controller/AdminController.java
+++ Castlerole/src/main/java/com/example/Castlerole/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@CrossOrigin
+@CrossOrigin()
 public class AdminController {
 
     @Autowired
