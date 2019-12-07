8140443ad48a4788e507196d7f6fafafcd35fe64
mlfalke <matthijsfalke@hotmail.com>
2019-12-07 02:05:20 +0100

    update Jasmine testing

--- castlerole-ng/src/app/components/logout/logout.component.spec.ts
+++ castlerole-ng/src/app/components/logout/logout.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogoutComponent } from './logout.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
@@ -8,6 +10,9 @@ describe('LogoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,
+        RouterTestingModule,
+      ],
       declarations: [ LogoutComponent ]
     })
     .compileComponents();
--- castlerole-ng/src/app/components/register/register.component.spec.ts
+++ castlerole-ng/src/app/components/register/register.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,6 +11,11 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
