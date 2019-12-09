714bedb779234816525770bddffbfdd866fd0bcd
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2019-12-09 10:09:25 +0100

    merg issues fix

---
+++ Castlerole/src/test/java/com/example/Castlerole/CastleroleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Castlerole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
