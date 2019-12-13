b14e345cee5b5e60e1742bda026e561b43363cff
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2019-12-13 18:36:07 +0100

    added click functionality to canvas and a game detail component

--- castlerole-ng/src/app/components/gameComponents/game-details/game-details.component.ts
+++ castlerole-ng/src/app/components/gameComponents/game-details/game-details.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Vector} from "../../../models/generic/Vector";
+import {GameServiceService} from "../../../services/game/game-service.service";
 
 @Component({
   selector: 'app-game-details',
   templateUrl: './game-details.component.html',
   styleUrls: ['./game-details.component.css']
 })
-export class GameDetailsComponent implements OnInit {
+export class GameDetailsComponent implements OnChanges {
 
-  constructor() { }
+  @Input() targetCoordinates: Vector;
 
-  ngOnInit() {
+  constructor(private gameService: GameServiceService) { }
+
+  async ngOnChanges(changes: SimpleChanges) {
+    await this.loadDetails();
+
+  }
+
+  loadDetails = async () => {
+    if (this.targetCoordinates.x > -1){
+
+    }
   }
 
 }
