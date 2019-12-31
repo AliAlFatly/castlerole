4fdf7d824e0a25b325d8e3fc9d103de57354c77b
yungkit9 <yungkithui@gmail.com>
2019-12-31 14:49:06 +0100

    .

--- Castlerole/src/test/java/com/example/Castlerole/Services/AdminServiceTest.java
+++ Castlerole/src/test/java/com/example/Castlerole/Services/AdminServiceTest.java
@@ -1,34 +1,58 @@
 package com.example.Castlerole.Services;
 
 import com.example.Castlerole.Config.ControllerTestConfig;
+import com.example.Castlerole.model.helpertypes.IntVector;
 import com.example.Castlerole.repository.NodeRepository;
 import com.example.Castlerole.service.AdminService;
+import com.example.Castlerole.service.JwtUserService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-public class AdminServiceTest extends ControllerTestConfig {
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AdminServiceTest {
 
     @Autowired
     private AdminService adminService;
 
-    @Autowired
+    @MockBean
     private NodeRepository nodeRepository;
+
+    @MockBean
+    private JwtUserService userService;
 
     @Test
     public void adminServiceTest() throws Exception {
 
+        when(userService.getXY()).thenReturn(new IntVector(23,25));
+
         int amount = 100;
+
 
         String[] types = {"forest", "lake", "mountain", "mine"};
 
-        adminService.generateNodes(amount);
+        var tekstt = adminService.generateNodes(amount);
 
-        Assert.assertEquals(nodeRepository.count(), amount);
+        //Assert.assertEquals(nodeRepository.count(), amount);
 
+        System.out.println(tekstt.toString());
         Iterable nodes = nodeRepository.findAll();
-        String nodesS = super.mapToJson(nodeRepository.findAll());
 
+        //String nodesS = super.mapToJson(nodeRepository.findAll());
+    Assert.assertEquals(tekstt,"done");
 
     }
 
