0283a1ab17c3ffa066c9803e4e925e85d376de44
mlfalke <matthijsfalke@hotmail.com>
2020-01-13 13:14:39 +0100

    Create AdminServiceTests.java

---
+++ Castlerole/src/test/java/com/example/Castlerole/service/AdminServiceTests.java
@@ -0,0 +1,49 @@
+package com.example.Castlerole.service;
+
+import com.example.Castlerole.model.Node;
+import com.example.Castlerole.model.User;
+import com.example.Castlerole.model.helpertypes.IntVector;
+import com.example.Castlerole.repository.NodeRepository;
+import com.example.Castlerole.repository.UserRepository;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.apache.maven.surefire.testset.TestRequest;
+import org.springframework.test.context.ActiveProfiles;
+
+@RunWith(MockitoJUnitRunner.class)
+@ActiveProfiles("test")
+public class AdminServiceTests {
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(userService.getXY())
+                .thenReturn(new IntVector(25,25));
+    }
+    @InjectMocks
+    private AdminService adminService;
+    @Mock
+    private NodeRepository nodeRepository;
+    @Mock
+    private JwtUserService userService;
+
+    @Test
+    public void whenValid_shouldReturnNodes() throws Exception {
+        int amount = 10;
+        String answer = adminService.generateNodes(1);
+        Assert.assertEquals(answer, "done");
+        System.out.println(answer);
+        //String answer = adminService.generateNodes(amount);
+
+
+
+    }
+
+
+}
