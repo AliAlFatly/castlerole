de721368b8752cfd1e2bcac89a286fda7d66eb23
yungkit9 <yungkithui@gmail.com>
2020-01-18 13:30:27 +0100

    .

--- Castlerole/src/main/java/com/example/Castlerole/repository/CityRepository.java
+++ Castlerole/src/main/java/com/example/Castlerole/repository/CityRepository.java
@@ -1,6 +1,7 @@
 package com.example.Castlerole.repository;
 
 import com.example.Castlerole.model.City;
+import com.example.Castlerole.model.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,13 +9,20 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Repository
 public interface CityRepository extends CrudRepository<City, Integer>{
 
     City findByOwner(String owner);
 
-    /*@Transactional
+    @Transactional
+    @Modifying
+    @Query("select c.casteLevel, c.woodworksLevel, c.mineLevel, c.forgeryLevel, c.barracksLevel, c.ovenLevel from City c, User u where u.coordinateX >= :minX and u.coordinateX <= :maxX and u.coordinateY >= :minY and u.coordinateY <= :maxY")
+    Optional<ArrayList<User>> getUsersInGrid(@Param("minX") int minX, @Param("maxX") int maxX, @Param("minY") int minY, @Param("maxY") int maxY);
+
+    @Transactional
     @Modifying
     @Query("UPDATE City c SET c.casteLevel = :amount WHERE c.owner = :owner")
     Integer updateCastleLevel(@Param("amount") int amount, @Param("owner") String owner);
